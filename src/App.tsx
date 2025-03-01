795c1e7e299828c038b6d8531bdf02a4e3b110ae
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-01 18:55:55 -0500

    Made the components list just one column on mobile.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <div className="flex flex-col items-center h-screen">
       <h1 className="text-3xl m-4">Welcome to bagoolCN!</h1>
       <h2 className="text-2xl m-3">Check out our components</h2>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid gird-cols-1 lg:grid-cols-3 gap-4">
         <Card>
           <Title> PrimaryButton </Title>
           <PrimaryButton> Press me </PrimaryButton>

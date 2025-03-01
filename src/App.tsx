941e56498e0058b254f70d67f606094c030dafb6
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-01 01:16:20 -0500

    Added a list of components.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,24 @@ export default function App() {
   return (
     <div className="flex flex-col items-center h-screen">
       <h1 className="text-3xl m-4">Welcome to bagoolCN!</h1>
+      <h2 className="text-2xl m-3">Check out our components</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <div> PrimaryButton </div>
+        <div> SecondaryButton </div>
+        <div> LogoButton </div>
+        <div> Header </div>
+        <div> Hero </div>
+        <div> Search </div>
+        <div> Slider </div>
+        <div> Card </div>
+        <div> Section </div>
+        <div> Dropdown </div>
+        <div> Carousel </div>
+        <div> Label </div>
+        <div> Link </div>
+        <div> Title </div>
+        <div> Subtitle </div>
+      </div>
     </div>
   );
 }

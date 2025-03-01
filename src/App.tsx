9f91155f130fb2dcbf6810ac82958c14234a553d
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-01 01:37:11 -0500

    Added Card component (just a div right now).

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
+import Card from "./card";
+
 export default function App() {
   return (
     <div className="flex flex-col items-center h-screen">
       <h1 className="text-3xl m-4">Welcome to bagoolCN!</h1>
       <h2 className="text-2xl m-3">Check out our components</h2>
       <div className="grid grid-cols-3 gap-4">
-        <div> PrimaryButton </div>
-        <div> SecondaryButton </div>
-        <div> LogoButton </div>
-        <div> Header </div>
-        <div> Hero </div>
-        <div> Search </div>
-        <div> Slider </div>
-        <div> Card </div>
-        <div> Section </div>
-        <div> Dropdown </div>
-        <div> Carousel </div>
-        <div> Label </div>
-        <div> Link </div>
-        <div> Title </div>
-        <div> Subtitle </div>
+        <Card> PrimaryButton </Card>
+        <Card> SecondaryButton </Card>
+        <Card> LogoButton </Card>
+        <Card> Header </Card>
+        <Card> Hero </Card>
+        <Card> Search </Card>
+        <Card> Slider </Card>
+        <Card> Card </Card>
+        <Card> Section </Card>
+        <Card> Dropdown </Card>
+        <Card> Carousel </Card>
+        <Card> Label </Card>
+        <Card> Link </Card>
+        <Card> Title </Card>
+        <Card> Subtitle </Card>
       </div>
     </div>
   );

7bf24ea37f294451781dbc7881c50b78602746ab
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-01 00:55:12 -0500

    Added TailwindCSS.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Welcome to bagoolCN!</h1>
+    <div className="flex flex-col items-center h-screen">
+      <h1 className="text-3xl m-4">Welcome to bagoolCN!</h1>
     </div>
   );
 }

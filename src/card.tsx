854017e66f8fa3cee158600950e24b7c28555e67
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-01 02:10:08 -0500

    Added some CSS to the Card.

--- src/card.tsx
+++ src/card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full border border-gray-100 bg-white shadow-lg p-6 rounded-md">
+    <div className="flex flex-col gap-4 w-full border border-gray-100 bg-white shadow-lg p-6 rounded-md">
       {children}
     </div>
   );

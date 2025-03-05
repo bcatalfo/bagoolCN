7ac7407f0b7779dda273a0a31c870cc19efde7ac
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-04 20:48:18 -0500

    Made the goober border a bit thicker.

--- src/button.tsx
+++ src/button.tsx
@@ -30,7 +30,7 @@ export default function Button({
       <div
         className={cn(
           {
-            "rounded-[8px] text-[15px] font-bold border-[1.5px] px-5 py-2 -translate-y-1 hover:-translate-y-1.5 active:-translate-y-0.5 mx-[-1.5px]":
+            "rounded-[8px] text-[15px] font-bold border-2 px-5 py-2 -translate-y-1 hover:-translate-y-1.5 active:-translate-y-0.5 mx-[-1.5px]":
               variant == "goober",
           },
           "flex items-center gap-4",

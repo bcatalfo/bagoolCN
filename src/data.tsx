eb0aeb95dcc15beae8e69235241850b0efc9d656
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-10 16:35:24 -0400

    Added the codeblock for the last button.

--- src/data.tsx
+++ src/data.tsx
@@ -414,5 +414,37 @@ export const buttonList: React.ReactNode[][] = [
     >
       I'm a button click on me!
     </Button>,
+    <>
+      <div className="flex">
+        <span className="text-blue-500">{`<Button `}</span>
+        <span className="text-orange-500">{`backgroundColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"orange"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`borderColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"black"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`textColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"black"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`variant`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"goober"`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`>`}</span>
+      </div>
+      <div className="flex ml-10">
+        <span className="text-slate-800">{`I'm a button click on me!`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`</Button>`}</span>
+      </div>
+    </>,
   ],
 ];

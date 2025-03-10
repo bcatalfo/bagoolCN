1b5e31d9b6bdfe2de14495245b991e29c1c1aecc
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-10 15:10:51 -0400

    Code block and changed text for another button.

--- src/data.tsx
+++ src/data.tsx
@@ -229,8 +229,47 @@ export const buttonList: React.ReactNode[][] = [
       borderColorOnHover="blueviolet"
       variant="action"
     >
-      I'm a button click on me!
+      I'm a button!
     </Button>,
+    <>
+      <div className="flex">
+        <span className="text-blue-500">{`<Button `}</span>
+        <span className="text-orange-500">{`backgroundColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"chartreuse"`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`<Button `}</span>
+        <span className="text-orange-500">{`backgroundColorOnHover`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"aquamarine"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`borderColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"black"`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`<Button `}</span>
+        <span className="text-orange-500">{`borderColorOnHover`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"blueviolet"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`variant`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"action"`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`>`}</span>
+      </div>
+      <div className="flex ml-10">
+        <span className="text-slate-800">{`I'm a button!`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`</Button>`}</span>
+      </div>
+    </>,
   ],
   [
     <Button

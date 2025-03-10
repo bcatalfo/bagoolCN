c38ce553f8a61167aaddec1be5bce0e72b7d829d
Benjamin Catalfo <bcatalfo@gmail.com>
2025-03-10 16:31:47 -0400

    Added codebox for another goober button.

--- src/data.tsx
+++ src/data.tsx
@@ -372,6 +372,38 @@ export const buttonList: React.ReactNode[][] = [
     >
       I'm a button click on me!
     </Button>,
+    <>
+      <div className="flex">
+        <span className="text-blue-500">{`<Button `}</span>
+        <span className="text-orange-500">{`backgroundColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"blue"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`borderColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"oklch(0.637 0.237 25.331)"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`textColor`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"white"`}</span>
+      </div>
+      <div className="flex ml-19">
+        <span className="text-orange-500">{`variant`}</span>
+        <span className="text-red-500">{`=`}</span>
+        <span className="text-green-500">{`"goober"`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`>`}</span>
+      </div>
+      <div className="flex ml-10">
+        <span className="text-slate-800">{`I'm a button click on me!`}</span>
+      </div>
+      <div className="flex">
+        <span className="text-blue-500">{`</Button>`}</span>
+      </div>
+    </>,
   ],
   [
     <Button
